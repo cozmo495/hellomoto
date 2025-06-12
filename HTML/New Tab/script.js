fb38d57d56259e34b6a618a9c020323f705aeac7
cozmoo <kindubindudindu@gmail.com>
2025-06-11 18:42:51 -0700

    0.1.2 (Intermediate Versions Saved on Desktop)

---
+++ HTML/New Tab/script.js
@@ -0,0 +1,17 @@
+  document.querySelector('#searchqueryform').addEventListener('submit', function(event) {
+      // This function is called when the form is submitted
+        // Listens for the submit event on the search form
+      console.log('Search button clicked'); // Log when the search is submitted for debugging
+      event.preventDefault(); // Prevent the form from submitting
+      // Get the value of the input field
+      const input = document.querySelector('#searchquery').value; // Get the value
+      console.log(`Searching for, ${input}!`); // Log the input value
+            document.getElementById('searchconfirmation').textContent = `Searching for: ${input}`; // Display the search confirmation before redirecting
+             console.log(`Search confirmed: ${input}`); // Log the search confirmation for debugging
+            // Simulate a delay before redirecting of 2 seconds
+            setTimeout(() => {
+                console.log(`Redirecting to DuckDuckGo with query: ${input}`);
+                // Redirect to DuckDuckGo with the search query
+            window.location.href = `https://duckduckgo.com/?q=${encodeURIComponent(input)}`;
+            }, 2000);
+    });
